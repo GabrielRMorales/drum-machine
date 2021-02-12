c31579b5c2ccb8f9ce604032f692a2d102962020
Gabriel Morales <grmorale@gmail.com>
2021-02-11 17:23:55 -0800

    Fulfill project requirements except for the styling

--- src/DrumApp.js
+++ src/DrumApp.js
@@ -1,4 +1,4 @@
-import {React, useEffect, Component} from "react";
+import {React, Component} from "react";
 import DrumPad from "./DrumPad";
 import q_file from "./music/Q.wav";
 import w_file from "./music/W.wav";
@@ -9,46 +9,60 @@ import d_file from "./music/D.wav";
 import z_file from "./music/Z.wav";
 import x_file from "./music/X.wav";
 import c_file from "./music/C.wav";
-const sources ={
-    q_file,
-    w_file,
-    e_file,
-    a_file,
-    s_file,
-    d_file,
-    z_file,
-    x_file,
-    c_file,
-}
 
 class DrumApp extends Component {
     constructor(props){
         super(props);
-
+        this.state={
+           currentSound: "",
+           sources: {
+           q_file,
+           w_file,
+           e_file,
+           a_file,
+           s_file,
+           d_file,
+           z_file,
+           x_file,
+           c_file,
+             }
+        }
+        this.handleClick=this.handleClick.bind(this);
     }
     componentDidMount(){
         window.addEventListener("keydown",(e)=>{
-            // let key=String.fromCharCode(e.key);
-            
-        let keyHit=(e.key).toLowerCase()+"_file";
-        console.log(keyHit);
-        if (typeof keyHit!=undefined){
-            let audio = new Audio(sources[keyHit]);
-        
-            audio.play();
+        let keyHit=(e.key).toLowerCase()+"_file";        
+        if (this.state.sources[keyHit]!==undefined){
+            let audioEl = document.getElementById(e.key.toUpperCase());
+            audioEl.play();
+            this.setState({
+                currentSound: `${e.key.toUpperCase()} note playing!`
+            });
         }               
 
         });
     }
     
+    handleClick(e){
+            // this.play();
+            let audio= e.target.childNodes[0];
+            //console.log(audio[0]);
+            audio.play();
+            this.setState({
+                currentSound: `${audio.id} note playing!`
+            });
+    }
+    
     render(){
         const drumPads=["Q", "W", "E", "A", "S", "D", "Z", "X", "C"].map((key,index)=>{
             let source = `${key.toLowerCase()}_file`;
-            return <DrumPad text={key} key={index+" "+key} source={sources[source]}/>
+          
+            return <DrumPad text={key} key={index+" "+key}
+                source={this.state.sources[source]} onClick={this.handleClick} />
         });
         return <main id="drum-machine">
-            <div id="display"></div>
             {drumPads}
+            <div id="display">{this.state.currentSound}</div>
         </main>
     }
 }
--- src/DrumPad.js
+++ src/DrumPad.js
@@ -1,15 +1,9 @@
 import React from "react";
 
-const DrumPad = ({text, source})=>{    
-   
+const DrumPad = ({text, source, onClick})=>{    
     return (
-        <button className="drum-pad" id={text+"-sound"} onClick={(e)=>{
-            // this.play();
-            let audio= e.target.childNodes[0];
-            //console.log(audio[0]);
-            audio.play()
-        }}>
-        <audio className="clip" id={text} src={source}></audio>{text}
+        <button className="drum-pad" id={text+"-sound"} onClick={onClick}>
+        <audio className="clip" id={text} src={source} ></audio>{text}
         </button>       
     )
 }
